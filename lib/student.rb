523ccedcbf97e74a8bcbfd1c1e876d014f102189
Erin Green <erin.y.green@gmail.com>
2018-11-17 20:00:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,4 +3,11 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
+  attr_accessor :name, :id 
+  
+  def initialize(:name, :id = nil)
+    @name = name
+    @id = id
+  end
+  
 end
